37e54e87efdf04e756893e7a329d4d00dbaf0610
Debayo Tomisin <debayotomisin07@gmail.com>
2021-01-19 14:15:57 -0800

    set up and pages

---
+++ src/pages/SingleRoom.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class SingleRoom extends Component {
+  constructor(props){
+    super()
+  }
+
+  render(){
+    return(
+        <div>
+          Hello from Single room page
+        </div>
+    )
+  }
+}

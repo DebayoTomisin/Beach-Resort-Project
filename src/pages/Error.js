aa77dc3d113fe6fa2bae4b7b1f4b70f13e4ba46b
Debayo Tomisin <debayotomisin07@gmail.com>
2021-01-20 14:21:13 -0800

    banner component and error page done

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import Hero from "../components/Hero"
+import {Link} from "react-router-dom"
+import Banner from "../components/Banner"
 
 const Error = () => {
   return(
-    <Hero/>
+    <Hero>
+      <Banner title="404" subtitle="pagenot found">
+        <Link to='/' className="btn-primary">return home</Link>
+      </Banner>
+    </Hero>
   )
 }
 

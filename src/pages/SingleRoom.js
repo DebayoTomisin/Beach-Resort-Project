3c900d10dd17dff39d61bce6631a2367f3943ba7
Debayo Tomisin <debayotomisin07@gmail.com>
2021-01-27 23:19:48 -0800

    singleroom page

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react'
+import defaultBcg from "../images/room-11.jpeg"
+import Hero from "../components/Hero"
+import Banner from "../components/Banner"
+import {Link} from "react-router-dom"
+import {RoomContext} from "../context"
 
 class SingleRoom extends Component {
   constructor(props){
-    super()
+    super(props)
+    console.log(this.props)
+  }
+
+  componentDidMount(){
+
   }
 
   render(){

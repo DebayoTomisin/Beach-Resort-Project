7b7eefcbcaf0140dee7e79a6bf2c496fdc621985
Debayo Tomisin <debayotomisin07@gmail.com>
2021-02-04 11:12:23 -0800

    rooms and rooms container component

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import Room from "./Room"
 
-export default function RoomList() {
+export default function RoomList({rooms}) {
+
+  if(rooms.length === 0){
+    return(
+      <div className="empty-search">
+        <h3>No rooms matched your search parameters</h3>
+      </div>
+    )
+  }
   return (
-    <div>
-      Hello from room list
-    </div>
+    <section className="roomslist">
+      <div className="roomlist-center">
+        {
+          rooms.map(room => {
+            return(
+              <Room key={room.id} room={room}/>
+            )
+          })
+        }
+      </div>
+    </section>
   )
 }

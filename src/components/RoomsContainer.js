ff08fc79e73d348cc9ae81c96eee07bb4a34b67f
Debayo Tomisin <debayotomisin07@gmail.com>
2021-02-04 10:28:34 -0800

    higher order component with context

--- src/components/RoomsContainer.js
+++ src/components/RoomsContainer.js
@@ -1,13 +1,28 @@
 import React from 'react'
 import RoomFilter from './RoomFilter'
 import RoomList from './RoomList'
+import { RoomConsumer } from "../context"
+import Loading from "./Loading"
 
 export default function RoomsContainer() {
   return (
-    <div>
-      Hello from rooms container
-      <RoomFilter/>
-      <RoomList/>
-    </div>
+    //instead of using this method, higher order components can be used as found in the cotext.js file
+    <RoomConsumer>
+      {
+        (value) => {
+          const { loading, sortedRooms, rooms} = value
+          if(loading){
+            return <Loading/>
+          }
+          return(
+            <div>
+              <RoomFilter rooms={rooms}/>
+              <RoomList rooms={sortedRooms}/>
+            </div>
+          )
+        }
+      }
+    </RoomConsumer>
+    
   )
 }
